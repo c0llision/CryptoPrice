d6b3409a75f7d3c415c0c98af07b4a1a8264cc97
c0llision <c0llision@protonmail.com>
2018-10-25 11:29:57 +0100

    Initial commit

---
+++ CryptoPrice/CryptoPrice/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CryptoPrice
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            updatePrice();
+        }
+        
+        public void updatePrice(string currency="BTC")
+        {
+            var client = new HttpClient();
+            var priceUrl = "https://apiv2.bitcoinaverage.com/convert/global?from=" + currency + "&to=EUR&amount=1";
+            var response = client.GetAsync(priceUrl).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string responseString = response.Content.ReadAsStringAsync().Result;
+                dynamic priceData = JsonConvert.DeserializeObject(responseString);
+
+                string price =  "€ " + priceData.price;
+                lblPrice.Text = price;
+            }
+
+        }
+    }
+}
