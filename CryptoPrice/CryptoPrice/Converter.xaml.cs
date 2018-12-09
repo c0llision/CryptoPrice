570d42853fb80170140c00ce4709c4d14fb63c92
c0llision <c0llision@protonmail.com>
2018-12-09 18:44:37 +0000

    Added converter class

--- CryptoPrice/CryptoPrice/Converter.xaml.cs
+++ CryptoPrice/CryptoPrice/Converter.xaml.cs
@@ -12,9 +12,16 @@ namespace CryptoPrice
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class Converter : ContentPage
 	{
+        btcConvert btcConvert = new btcConvert();
+
 		public Converter ()
 		{
 			InitializeComponent ();
 		}
-	}
+
+        private void updateConversion(object sender, TextChangedEventArgs e)
+        {
+            btcConvert.amount = txtFrom.Text;
+        }
+    }
 }
---
+++ CryptoPrice/CryptoPrice/btcConvert.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace CryptoPrice
+{
+    class btcConvert
+    {
+
+        private HttpClient client = new HttpClient();
+        public string fromCurrency = "eur";
+        public string toCurrency = "btc";
+        public string amount = "1";
+        public string convertAmount = "1";
+
+        public void convert()
+        {
+
+            var priceUrl = "https://apiv2.bitcoinaverage.com/convert/global?from=" + fromCurrency + "&to=" + toCurrency + "&amount=" + amount;
+            var response = client.GetAsync(priceUrl).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string responseString = response.Content.ReadAsStringAsync().Result;
+                dynamic priceData = JsonConvert.DeserializeObject(responseString);
+
+                convertAmount = priceData.price;
+            }
+
+        }
+    }
+}
