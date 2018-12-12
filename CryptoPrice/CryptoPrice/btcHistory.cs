6224d49c49f463dd5857ef425f416729942e2e0e
c0llision <c0llision@protonmail.com>
2018-12-12 19:02:17 +0000

    Added history class

---
+++ CryptoPrice/CryptoPrice/btcHistory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace CryptoPrice
+{
+    class btcHistory
+    {
+        private HttpClient client = new HttpClient();
+        private int NUM_DAYS = 7;
+        public string currency = "BTC";
+        public string[] dates = new string[NUM_DAYS];
+        public string[] prices = new string[NUM_DAYS];
+
+        public void update()
+        {
+            
+        }
+    }
+}
