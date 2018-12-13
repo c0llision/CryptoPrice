d4bb252869e0ee750e26de58a7b092a1710da1d1
c0llision <c0llision@protonmail.com>
2018-12-13 19:28:25 +0000

    History page logic

--- CryptoPrice/CryptoPrice/PriceHistory.xaml.cs
+++ CryptoPrice/CryptoPrice/PriceHistory.xaml.cs
@@ -12,9 +12,45 @@ namespace CryptoPrice
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class PriceHistory : ContentPage
 	{
+        btcHistory btcHistory = new btcHistory();
 		public PriceHistory ()
 		{
 			InitializeComponent ();
+            update();
+
 		}
-	}
+        
+        public void update()
+        {
+            int index = pckCurrency.SelectedIndex;
+
+            if(index != -1)
+            {
+                btcHistory.currency = pckCurrency.Items[index];
+            }
+
+            btcHistory.update();
+
+            date1.Text = btcHistory.dates[0];
+            date2.Text = btcHistory.dates[1];
+            date3.Text = btcHistory.dates[2];
+            date4.Text = btcHistory.dates[3];
+            date5.Text = btcHistory.dates[4];
+            date6.Text = btcHistory.dates[5];
+            date7.Text = btcHistory.dates[6];
+
+            price1.Text = btcHistory.prices[0];
+            price2.Text = btcHistory.prices[1];
+            price3.Text = btcHistory.prices[2];
+            price4.Text = btcHistory.prices[3];
+            price5.Text = btcHistory.prices[4];
+            price6.Text = btcHistory.prices[5];
+            price7.Text = btcHistory.prices[6];
+        }
+
+        private void pckCurrency_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            update();
+        }
+    }
 }
--- CryptoPrice/CryptoPrice/btcHistory.cs
+++ CryptoPrice/CryptoPrice/btcHistory.cs
@@ -9,14 +9,29 @@ namespace CryptoPrice
     class btcHistory
     {
         private HttpClient client = new HttpClient();
-        private int NUM_DAYS = 7;
         public string currency = "BTC";
-        public string[] dates = new string[NUM_DAYS];
-        public string[] prices = new string[NUM_DAYS];
+        public string[] dates = new string[7];
+        public string[] prices = new string[7];
 
         public void update()
         {
-            
+
+            var url = "https://apiv2.bitcoinaverage.com/indices/global/history/" + currency + "EUR?period=alltime&format=json";
+            var response = client.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string responseString = response.Content.ReadAsStringAsync().Result;
+                dynamic data = JsonConvert.DeserializeObject(responseString);
+
+                for (int i=0; i < 7; i++)
+                {
+                    string date = data[i].time;
+                    string price = data[i].average;
+                    dates[i] = date.Trim().Substring(0, 10);
+                    prices[i] = price;
+                }
+            }
+
         }
     }
 }
