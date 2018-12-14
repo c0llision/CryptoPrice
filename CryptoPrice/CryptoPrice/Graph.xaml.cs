03ff5a4828d56272a0446a31f12787381c03ec56
c0llision <c0llision@protonmail.com>
2018-12-14 19:50:33 +0000

    Graph demo working

--- CryptoPrice/CryptoPrice/Graph.xaml.cs
+++ CryptoPrice/CryptoPrice/Graph.xaml.cs
@@ -16,13 +16,35 @@ namespace CryptoPrice
 		public Graph ()
 		{
 			InitializeComponent ();
+            barChart.Chart = new BarChart { Entries = _entries };
 		}
 
         private readonly List<Microcharts.Entry> _entries = new List<Microcharts.Entry>()
         {
             new Microcharts.Entry(200)
             {
-            }
+                Label = "Mon",
+                ValueLabel = "200",
+                Color = SkiaSharp.SKColor.Parse("#0000ff"),
+            },
+            new Microcharts.Entry(210)
+            {
+                Label = "Mon",
+                ValueLabel = "210",
+                Color = SkiaSharp.SKColor.Parse("#0000ff"),
+            },
+            new Microcharts.Entry(120)
+            {
+                Label = "Mon",
+                ValueLabel = "120",
+                Color = SkiaSharp.SKColor.Parse("#0000ff"),
+            },
+            new Microcharts.Entry(100)
+            {
+                Label = "Mon",
+                ValueLabel = "100",
+                Color = SkiaSharp.SKColor.Parse("#0000ff"),
+            },
         };
 	}
 }
