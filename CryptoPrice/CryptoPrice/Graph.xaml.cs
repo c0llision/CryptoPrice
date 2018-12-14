fdc35ab97e6015e5a3de510ace78090adf244f2f
c0llision <c0llision@protonmail.com>
2018-12-14 19:44:27 +0000

    Added Microcharts NuGet package and created Graphs page

---
+++ CryptoPrice/CryptoPrice/Graph.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Microcharts;
+
+namespace CryptoPrice
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Graph : ContentPage
+	{
+		public Graph ()
+		{
+			InitializeComponent ();
+		}
+
+        private readonly List<Microcharts.Entry> _entries = new List<Microcharts.Entry>()
+        {
+            new Microcharts.Entry(200)
+            {
+            }
+        };
+	}
+}
